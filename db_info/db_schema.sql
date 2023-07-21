8371eecf51f78eeda7f152160cd0747455f4c768
Julia Stoyanovich <stoyanovich@nyu.edu>
2023-07-21 17:47:51 -0400

    Update db_schema.sql
    - changed "Takes" to "Students_Take_Tests"
    - changed test_centers_name attribute to test_center_name
    - added "not null" constraints to several attributes to implement participation (e.g., "Every student is located in exactly one location" requires that location_condifier in Students be made "not null", similarly for other cases)
    - note that there is no need to add "not null" for attributes that are part of the primary key; each attribute that forms part of a primary key is not null

--- db_info/db_schema.sql
+++ db_info/db_schema.sql
@@ -7,29 +7,30 @@ create table Organizations (
 );
 
 create table Years (
-    year year primary key
+    year year primary key 
 );
 
 create table Schools (
     school_name varchar(256)  primary key,
-    organization_name varchar(256),
-    location_codifier integer,
+    organization_name varchar(256) not null,
+    location_codifier integer not null,
     foreign key (organization_name) references Organizations(organization_name),
     foreign key (location_codifier) references Locations(location_codifier)
 );
 
 create table Schools_Stats (
     id integer primary key,
-    school_name varchar(256),
-    year year,
+    school_name varchar(256) not null,
+    year year not null,
     foreign key (school_name) references Schools(school_name),
-    foreign key (year) references Years(year)
+    foreign key (year) references Years(year),
+    unique (school_name, year)
 );
 
 create table Students (
     student_id integer primary key,
     school_name varchar(256),
-    location_codifier integer,
+    location_codifier integer not null,
     foreign key (school_name) references Schools(school_name),
     foreign key (location_codifier) references Locations(location_codifier)
 );
@@ -41,20 +42,20 @@ create table Tests (
 );
 
 create table Test_Centers (
-    test_centers_name varchar(256)  primary key,
-    location_codifier integer,
+    test_center_name varchar(256)  primary key,
+    location_codifier integer not null,
     foreign key (location_codifier) references Locations(location_codifier)
 );
 
-create table takes (
-    test_centers_name varchar(256),
+create table Students_Take_Tests (
+    test_center_name varchar(256),
     test_type varchar(256),
     test_subject varchar(256),
     student_id integer,
     year year,
     score float(1),
-    primary key(test_centers_name, test_type, test_subject, student_id, year),
-    foreign key (test_centers_name) references Test_Centers(test_centers_name),
+    primary key(test_center_name, test_type, test_subject, student_id, year),
+    foreign key (test_center_name) references Test_Centers(test_center_name),
     foreign key (test_type, test_subject) references Tests(test_type, test_subject),
     foreign key (student_id) references Students(student_id),
     foreign key (year) references Years(year)
