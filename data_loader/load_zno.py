ab7d3ca16cb12840d1118e07add2161db406fdf3
Tetiana Zakharchenko <smyarga@gmail.com>
2023-11-09 11:04:09 +0200

    refine code for load_zno

--- data_loader/load_zno.py
+++ data_loader/load_zno.py
@@ -1,3 +1,5 @@
+'''module to load datasets from open data portal'''
+
 import os
 import requests
 import pandas as pd
@@ -19,7 +21,7 @@ def download_and_extract(url, datadir, remote_fname, file_name, delete_download=
         os.rename(os.path.join(datadir, filename), os.path.join(datadir, file_name))
         # get filename dir and delete it
         os.rmdir(os.path.join(datadir, filename.split('/')[0]))
-    
+
     if delete_download and download_path != os.path.join(datadir, file_name):
         os.remove(download_path)
 
@@ -32,7 +34,7 @@ def initialize_and_download(datadir, year, download=False): # TODO: typehinting
         file_name = f'Odata{year}File.csv'
     else:
         file_name = f'OpenData{year}.csv'
-    
+
     # Assume is the path exists and is a file, then it has been downloaded
     # correctly
     file_path = os.path.join(datadir, file_name)
@@ -40,7 +42,7 @@ def initialize_and_download(datadir, year, download=False): # TODO: typehinting
         return file_path
     if not download:
         raise FileNotFoundError(f'Could not find {year} ZNO data for in {datadir}. Call get_data with download=True to download the dataset.')
-    
+
     print(f'Downloading data for {year} data...')
 
     if int(year) >= 2022:
@@ -54,8 +56,8 @@ def initialize_and_download(datadir, year, download=False): # TODO: typehinting
     try:
         download_and_extract(url, datadir, remote_fname, file_name, delete_download=True)
     except Exception as e:
-        print(f'\n{os.path.join(datadir, remote_fname)} may be corrupted. Please try deleting it and rerunning this command.\n')
-        print(f'Exception: ', e)
+        print(f'\n{os.path.join(datadir, remote_fname)} may be corrupted. Try deleting it and rerunning this command.')
+        print('Exception: ', e)
         # TODO: rewrite print as logging
     return file_path
 
@@ -67,7 +69,7 @@ def load_zno(root_dir, year=2016, download=False): # TODO: typehinting
     if int(year) < 2016:
         raise ValueError('Year must be >= 2016')
     # TODO: raise error when year>current_year
-    
+
     base_datadir = os.path.join(root_dir, str(year))
     os.makedirs(base_datadir, exist_ok=True)
     file_name = initialize_and_download(base_datadir, year, download=download)
